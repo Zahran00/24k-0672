17d28ba2bd8b66983690cb2f7935861e9583a9e8
Zahran00 <zahranc03@gmail.com>
2024-10-09 00:44:19 +0500

    Update TASK_3.c

--- PF LAB  Assignments /LAB 6 Assignments/TASK_3.c
+++ PF LAB  Assignments /LAB 6 Assignments/TASK_3.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 
 int main(){
-	int num,i;
+	int num,isPrime=1;
 	printf("Enter Number : ");
-	scanf("%d",&num);
-	
-	for(i=0; num!=0; i++){
-		num/=10;
+	scanf("%d", &num);
+		if (num == 0 || num == 1){
+		    isPrime = 2;
+}	
+	for(int i=2; i<num; i++){
+
+		if(num%i==0){
+			isPrime=0;
+		}
 	}
-	if(i==1) printf("The Number is Single digit");
-	else printf("The Number is Multiple digit");
+if (isPrime == 2){
+	    printf("Entered number is neither prime nor composite");}
+
+	else if(isPrime == 1) {
+	printf( "Entered number is prime");
+}
+	else{ printf("Entered number is composite");
+}
 }
