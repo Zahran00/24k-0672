b0f2a55817c8dfd05e3b7c376443531fbc8c4145
Zahran00 <zahranc03@gmail.com>
2024-10-12 23:51:39 +0500

    Update TASK_4.c

--- PF LAB  Assignments /Lab 5 Assignment/TASK_4.c
+++ PF LAB  Assignments /Lab 5 Assignment/TASK_4.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+int main() {
+    int age;
+    char status[3];
+
+    printf("Input your age and citizenship (e.g 18 US)\n");
+    scanf("%d", &age);
+    scanf("%s", status);
+
+    if (age > 17 && status[0] == 'U' && status[1] == 'S') {
+ 
+        printf("You are eligible to vote\n");
+    } 
+    else {
+        printf("You are not eligible to vote\n");
+    }
+}
 
