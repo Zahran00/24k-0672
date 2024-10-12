ca8a1dbd4d612c8b654c992aa68691725c6dda46
Zahran00 <zahranc03@gmail.com>
2024-10-12 23:56:55 +0500

    Update TASK_5.c

--- PF LAB  Assignments /Lab 5 Assignment/TASK_5.c
+++ PF LAB  Assignments /Lab 5 Assignment/TASK_5.c
@@ -1,1 +1,9 @@
+#include <stdio.h>
+int main() {
+int num1,num2;
+printf("Enter both the numbers: \n");
+scanf("%d", &num1);
+scanf("%d", &num2);
 
+num1 > num2 ? printf("%d is greater than %d \n",num1,num2) : printf("%d is greater than %d \n",num2,num1);
+}
