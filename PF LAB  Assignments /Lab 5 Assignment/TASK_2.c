c6f73c930d29eea1537b290cd6c56f366d348bf0
Zahran00 <zahranc03@gmail.com>
2024-10-12 23:35:44 +0500

    Update TASK_2.c

--- PF LAB  Assignments /Lab 5 Assignment/TASK_2.c
+++ PF LAB  Assignments /Lab 5 Assignment/TASK_2.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+int main() {
+
+    int n;
+    printf("Input a number ");
+    scanf("%d",&n);
+    
+    if(n > 0) {
+        if (n % 2 == 0){
+            printf("Number is positive and even");
+        }
+            else{ 
+            printf("Number is positive and odd");
+            
+        }
+    }
+    else if (n < 0) {
+            printf("Number is negative");
+            
+        }
+    else if (n == 0) {
+            printf("Number is Zero");
+        }
+    else {
+            printf("Invalid Input,Try inputting a number this time.");
+        }
+    }
