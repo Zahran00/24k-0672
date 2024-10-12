fc15ac21e32552435233a7a8f5696a702df62203
Zahran00 <zahranc03@gmail.com>
2024-10-13 00:03:37 +0500

    Update TASK_6.c

--- PF LAB  Assignments /Lab 5 Assignment/TASK_6.c
+++ PF LAB  Assignments /Lab 5 Assignment/TASK_6.c
@@ -1,1 +1,9 @@
+#include <stdio.h>
+int main() {
+int num;
+printf("Enter the number: \n");
+scanf("%d", &num);
 
+num > 0 ? printf("%d is positive \n",num) : (num < 0 ? printf("%d is negative \n",num) : printf("%d is zero",num));
+
+}
