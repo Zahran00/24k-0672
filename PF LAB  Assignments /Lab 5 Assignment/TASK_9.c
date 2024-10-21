3a0422d77fea8c89dac8eabb160695395d3643c8
Zahran00 <zahranc03@gmail.com>
2024-10-21 20:01:04 +0500

    Update TASK_9.c

--- PF LAB  Assignments /Lab 5 Assignment/TASK_9.c
+++ PF LAB  Assignments /Lab 5 Assignment/TASK_9.c
@@ -1,1 +1,17 @@
-
+//Create a program that counts the number of 1s in the binary representation of a number.
+#include <stdio.h>
+int main(){
+    int year;
+    printf("Enter year: ");
+    scanf("%d",&year);
+    if (year % 4 ==0 & year % 100 != 0){
+        printf("It is a leap year");
+    }
+        else if (year % 400 == 0){
+            printf("It is a leap year");
+        }
+            else {
+                printf("It is not a leap year");
+        }
+    
+}
