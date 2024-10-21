65df38fd87cf94eeae64a727758bc47070b3db37
Zahran00 <zahranc03@gmail.com>
2024-10-21 19:55:59 +0500

    Update TASK_8.c

--- PF LAB  Assignments /Lab 5 Assignment/TASK_8.c
+++ PF LAB  Assignments /Lab 5 Assignment/TASK_8.c
@@ -1,1 +1,15 @@
-
+//Create a program that counts the number of 1s in the binary representation of a number.
+#include <stdio.h>
+int main(){
+    int num,check,count=0;
+    printf("Enter any binary number ");
+    scanf("%d",&num);
+    while(num > 0) {
+        check = num % 10;
+        if (check == 1){
+            count++;
+        }  
+        num/= 10;
+    }
+    printf("Amount of 1s in binary number: %d",count);
+}
