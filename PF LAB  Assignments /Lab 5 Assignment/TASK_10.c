d9715d76400df60b00c4901a0b4ae351202ce385
Zahran00 <zahranc03@gmail.com>
2024-10-21 19:40:15 +0500

    Update TASK_10.c

--- PF LAB  Assignments /Lab 5 Assignment/TASK_10.c
+++ PF LAB  Assignments /Lab 5 Assignment/TASK_10.c
@@ -1,1 +1,15 @@
+//Create a program that calculates the sum of digits of a number until the result is a single digit (e.g., 123 -> 6).
 
+#include <stdio.h>
+
+int main(){
+    int num;
+    int sum=0;
+    printf("Enter number: ");
+    scanf("%d", &num);
+    while (num>0){
+        sum+= num%10;
+        num/=10;
+    }
+    printf("Sum: %d", sum);
+}
