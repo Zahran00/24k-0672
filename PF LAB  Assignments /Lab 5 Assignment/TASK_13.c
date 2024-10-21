d7aa35af3e191a24e9f54d3695554658a161c80d
Zahran00 <zahranc03@gmail.com>
2024-10-21 19:44:14 +0500

    Update TASK_13.c

--- PF LAB  Assignments /Lab 5 Assignment/TASK_13.c
+++ PF LAB  Assignments /Lab 5 Assignment/TASK_13.c
@@ -1,1 +1,31 @@
+//Write a program that uses bitwise operators to perform encryption and decryption of a character.
 
+#include <stdio.h>
+
+int main(){
+    char ch;
+    int key= 10;
+    char encrypt, decrypt;
+
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    int option;
+    printf("Select option:\n 1)Encrypt \n 2)Decrypt\n");
+    scanf("%d", &option);
+
+    switch (1)
+    {
+    case 1:
+        encrypt= ch^10;
+        printf("Encrypted character: %c\n", encrypt);
+        break;
+    case 2:    
+        decrypt= ch^10; 
+        printf("Decrypted character: %c\n", decrypt);
+    default:
+        printf("Invalid option\n");
+        break;
+    }
+    
+}
